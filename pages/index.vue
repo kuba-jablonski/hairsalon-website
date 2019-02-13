19d37deb0ffef949cc979cbf5d0f24cde21d18f5
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2019-02-13 18:49:16 +0100

    style services section desktop

--- pages/index.vue
+++ pages/index.vue
@@ -41,40 +41,44 @@
             </base-button>
           </div>
         </section>
-        <section>
-          <h2>Our Services</h2>
-          <p>The House of Hair Salon & Spa, incididunt ut labore et dolore magna aliqua.</p>
-          <div class="card">
-            <img
-              src="~/assets/images/s1.png"
-              alt=""
-            >
-            <h4>Coloring</h4>
-            <p>Ut enim ad minim veniam, quis trud exercitation...</p>
+        <section class="services">
+          <div class="services__heading">
+            <h2>Our Services</h2>
+            <p>The House of Hair Salon & Spa, incididunt ut labore et dolore magna aliqua.</p>
           </div>
-          <div class="card">
-            <img
-              src="~/assets/images/s3.png"
-              alt=""
-            >
-            <h4>Haircut</h4>
-            <p>Consectetur adipisicing elit, sed doe eiusmod.</p>
-          </div>
-          <div class="card">
-            <img
-              src="~/assets/images/s4.png"
-              alt=""
-            >
-            <h4>Hairstyle</h4>
-            <p>Nemo enim ipsam voluptatem quia voluptas</p>
-          </div>
-          <div class="card">
-            <img
-              src="~/assets/images/s3.png"
-              alt=""
-            >
-            <h4>Coloring</h4>
-            <p>Ut enim ad minim veniam, quis trud exercitation...</p>
+          <div class="services__cards">
+            <div class="card">
+              <img
+                src="~/assets/images/s1.png"
+                alt=""
+              >
+              <h4>Coloring</h4>
+              <p>Ut enim ad minim veniam, quis trud exercitation...</p>
+            </div>
+            <div class="card">
+              <img
+                src="~/assets/images/s3.png"
+                alt=""
+              >
+              <h4>Haircut</h4>
+              <p>Consectetur adipisicing elit, sed doe eiusmod.</p>
+            </div>
+            <div class="card">
+              <img
+                src="~/assets/images/s4.png"
+                alt=""
+              >
+              <h4>Hairstyle</h4>
+              <p>Nemo enim ipsam voluptatem quia voluptas</p>
+            </div>
+            <div class="card">
+              <img
+                src="~/assets/images/s3.png"
+                alt=""
+              >
+              <h4>Coloring</h4>
+              <p>Ut enim ad minim veniam, quis trud exercitation...</p>
+            </div>
           </div>
         </section>
       </div>
@@ -118,7 +122,7 @@
   background: white;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  padding: 5rem 0;
+  padding: 8rem 0;
   align-items: center;
   border-bottom: 1px solid #e6e6e6;
 
@@ -152,6 +156,35 @@
 
   &__btn {
     margin-top: 4rem;
+  }
+}
+
+.services {
+  text-align: center;
+  background-color: #fff;
+  padding: 8rem 0;
+
+  &__heading {
+    margin-bottom: 5rem;
+  }
+
+  &__cards {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 2rem;
+  }
+}
+
+.card {
+  border: 1px solid #e6e6e6;
+  padding: 4rem 2rem;
+
+  img {
+    margin-bottom: 1.5rem;
+  }
+
+  p {
+    margin: 0;
   }
 }
 </style>
