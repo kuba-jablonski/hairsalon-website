4936e5944c362dbb3038eb65634e4142fa42c8e4
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2019-04-26 10:00:59 +0200

    style personnel section - desktop

--- components/Home/SectionPersonnel.vue
+++ components/Home/SectionPersonnel.vue
@@ -1,21 +1,22 @@
 <template>
-  <section class="personnel">
-    <div class="personnel__wrapper">
+  <div>
+    <section class="personnel">
+      <div class="thumbnail" />
       <div class="container">
-        <div class="personnel__one">
-          <img
-            class="personnel__bgimage"
-            src="~/assets/images/14.png"
-          >
+        <div class="personnel__top">
           <div class="personnel__text">
             <h2>Our Experts</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro eos ea vel debitis ad, officiis natus maiores consequatur. Ea, nesciunt.</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis nostrud exercitation ullamco laboris nisi ut aliquip ex.
+            </p>
+            <div class="personnel__cert">
+              <img src="~/assets/images/certificate-1.webp">
+              <img src="~/assets/images/certificate-2.webp">
+              <img src="~/assets/images/certificate-3.webp">
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="personnel__two">
-      <div class="container">
+
         <div class="personnel__cards">
           <div class="personnel__card">
             <img src="~/assets/images/10.jpg">
@@ -25,83 +26,108 @@
             </div>
           </div>
           <div class="personnel__card">
-            <img src="~/assets/images/10.jpg">
+            <img src="~/assets/images/11.jpg">
             <div class="personnel__card-text">
-              <h4>Mila Hartley</h4>
-              <p>Hairdresser</p>
+              <h4>Teigan Duran</h4>
+              <p>Stylist</p>
             </div>
           </div>
           <div class="personnel__card">
-            <img src="~/assets/images/10.jpg">
+            <img src="~/assets/images/12.jpg">
             <div class="personnel__card-text">
-              <h4>Mila Hartley</h4>
-              <p>Hairdresser</p>
+              <h4>Tanya Ramsey</h4>
+              <p>Hairstylist</p>
             </div>
           </div>
           <div class="personnel__card">
-            <img src="~/assets/images/10.jpg">
+            <img src="~/assets/images/13.jpg">
             <div class="personnel__card-text">
-              <h4>Mila Hartley</h4>
-              <p>Hairdresser</p>
+              <h4>Donna Carr</h4>
+              <p>Barber</p>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
-  <!-- <div class="personnel__wrapper">
+    </section>
     <div class="container">
-      <section class="personnel" />
-
+      <div class="section-break" />
     </div>
-  </div> -->
+  </div>
 </template>
 
 <style lang="scss" scoped>
-.personnel {
-  padding-top: 0;
-  // display: grid;
-  // grid-template-columns: 1fr 1fr;
-  // grid-gap: 5rem;
+.section-break {
+  border-bottom: 1px solid #e6e6e6;
+}
+.thumbnail {
+  background-image: url('~assets/images/14.png');
+  position: absolute;
+  width: 55.5rem;
+  height: 70%;
+  top: 0;
+  right: 50%;
+  z-index: -1;
+}
 
-  &__one {
-    // background-color: #f6f9f9;
-    background-color: red;
+.personnel {
+  position: relative;
+  z-index: 1;
+
+  &::before {
+    position: absolute;
+    width: 100%;
+    height: 70%;
+    content: '';
+    top: 0;
+    right: 0;
+    background-color: #f6f9f9;
+    z-index: -1;
+  }
+
+  &__top {
     display: grid;
     grid-template-columns: 1fr 1fr;
-    grid-gap: 5rem;
-  }
-
-  &__two {
-    // transform: translateY(-50%);
-  }
-
-  &__wrapper {
-    // background-color: #f6f9f9;
-    background-color: black;
-  }
-
-  &__bgimage {
-    display: block;
+    grid-gap: 2rem;
   }
 
   &__text {
-    margin-top: 8rem;
+    grid-column: 2;
+    z-index: 1;
+
+    p {
+      margin-bottom: 4rem;
+    }
+  }
+
+  &__cert {
+    margin-bottom: 4rem;
+
+    & > * {
+      margin-right: 2rem;
+    }
   }
 
   &__cards {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
-    grid-gap: 2rem;
-  }
-
-  &__card {
-    border: 1px solid black;
+    grid-gap: 3rem;
 
     img {
       width: 100%;
-      display: block;
+      filter: grayscale(1);
+
+      &:hover {
+        filter: none;
+      }
     }
+  }
+
+  &__card {
+    border: 1px solid #e5e5e5;
+  }
+
+  &__card-text {
+    padding: 2rem;
   }
 }
 </style>
