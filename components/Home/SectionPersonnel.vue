18dc8c6fba43d3f19048d5f6bcefa81ac868b144
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2019-04-26 11:14:53 +0200

    style personnel cards

--- components/Home/SectionPersonnel.vue
+++ components/Home/SectionPersonnel.vue
@@ -19,28 +19,92 @@
 
         <div class="personnel__cards">
           <div class="personnel__card">
-            <img src="~/assets/images/10.jpg">
+            <div class="personnel__image-box">
+              <div class="personnel__overlay">
+                <div class="personnel__icon-bg">
+                  <facebook-icon class="personnel__icon" />
+                </div>
+                <div class="personnel__icon-bg">
+                  <twitter-icon class="personnel__icon" />
+                </div>
+                <div class="personnel__icon-bg">
+                  <linkedin-icon class="personnel__icon" />
+                </div>
+                <div class="personnel__icon-bg">
+                  <dribbble-icon class="personnel__icon" />
+                </div>
+              </div>
+              <img src="~/assets/images/10.jpg">
+            </div>
             <div class="personnel__card-text">
               <h4>Mila Hartley</h4>
               <p>Hairdresser</p>
             </div>
           </div>
           <div class="personnel__card">
-            <img src="~/assets/images/11.jpg">
+            <div class="personnel__image-box">
+              <div class="personnel__overlay">
+                <div class="personnel__icon-bg">
+                  <facebook-icon class="personnel__icon" />
+                </div>
+                <div class="personnel__icon-bg">
+                  <twitter-icon class="personnel__icon" />
+                </div>
+                <div class="personnel__icon-bg">
+                  <linkedin-icon class="personnel__icon" />
+                </div>
+                <div class="personnel__icon-bg">
+                  <dribbble-icon class="personnel__icon" />
+                </div>
+              </div>
+              <img src="~/assets/images/11.jpg">
+            </div>
             <div class="personnel__card-text">
               <h4>Teigan Duran</h4>
               <p>Stylist</p>
             </div>
           </div>
           <div class="personnel__card">
-            <img src="~/assets/images/12.jpg">
+            <div class="personnel__image-box">
+              <div class="personnel__overlay">
+                <div class="personnel__icon-bg">
+                  <facebook-icon class="personnel__icon" />
+                </div>
+                <div class="personnel__icon-bg">
+                  <twitter-icon class="personnel__icon" />
+                </div>
+                <div class="personnel__icon-bg">
+                  <linkedin-icon class="personnel__icon" />
+                </div>
+                <div class="personnel__icon-bg">
+                  <dribbble-icon class="personnel__icon" />
+                </div>
+              </div>
+              <img src="~/assets/images/12.jpg">
+            </div>
             <div class="personnel__card-text">
               <h4>Tanya Ramsey</h4>
               <p>Hairstylist</p>
             </div>
           </div>
           <div class="personnel__card">
-            <img src="~/assets/images/13.jpg">
+            <div class="personnel__image-box">
+              <div class="personnel__overlay">
+                <div class="personnel__icon-bg">
+                  <facebook-icon class="personnel__icon" />
+                </div>
+                <div class="personnel__icon-bg">
+                  <twitter-icon class="personnel__icon" />
+                </div>
+                <div class="personnel__icon-bg">
+                  <linkedin-icon class="personnel__icon" />
+                </div>
+                <div class="personnel__icon-bg">
+                  <dribbble-icon class="personnel__icon" />
+                </div>
+              </div>
+              <img src="~/assets/images/13.jpg">
+            </div>
             <div class="personnel__card-text">
               <h4>Donna Carr</h4>
               <p>Barber</p>
@@ -55,10 +119,28 @@
   </div>
 </template>
 
+<script>
+import FacebookIcon from '~/assets/images/facebook.svg?inline'
+import TwitterIcon from '~/assets/images/twitter.svg?inline'
+import LinkedinIcon from '~/assets/images/linkedin.svg?inline'
+import DribbbleIcon from '~/assets/images/dribbble.svg?inline'
+
+export default {
+  components: {
+    FacebookIcon,
+    TwitterIcon,
+    LinkedinIcon,
+    DribbbleIcon
+  }
+}
+</script>
+
+
 <style lang="scss" scoped>
 .section-break {
   border-bottom: 1px solid #e6e6e6;
 }
+
 .thumbnail {
   background-image: url('~assets/images/14.png');
   position: absolute;
@@ -115,19 +197,76 @@
     img {
       width: 100%;
       filter: grayscale(1);
-
-      &:hover {
-        filter: none;
-      }
+      transition: filter 0.3s;
+      -webkit-transition: 0.3s -webkit-filter;
+      -moz-transition: 0.3s -moz-filter;
+      -moz-transition: 0.3s filter;
+      -ms-transition: 0.3s -ms-filter;
+      -o-transition: 0.3s -o-filter;
     }
   }
 
   &__card {
     border: 1px solid #e5e5e5;
+
+    &:hover {
+      img {
+        filter: none;
+      }
+
+      .personnel__overlay {
+        opacity: 1;
+      }
+    }
   }
 
   &__card-text {
     padding: 2rem;
+  }
+
+  &__image-box {
+    position: relative;
+  }
+
+  &__overlay {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(white, 0.7);
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    transition: all 0.3s;
+    opacity: 0;
+    z-index: 1;
+  }
+
+  &__icon-bg {
+    background-color: transparent;
+    height: 4.5rem;
+    width: 4.5rem;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.3s;
+    cursor: pointer;
+
+    &:hover {
+      background-color: $color-primary;
+
+      .personnel__icon {
+        fill: white;
+      }
+    }
+  }
+
+  &__icon {
+    height: 1.5rem;
+    width: 1.5rem;
+    transition: fill 0.3s;
   }
 }
 </style>
