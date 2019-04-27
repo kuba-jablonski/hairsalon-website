7bb27965c1706b3ae487bdd2d2eab02d0c05f74f
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2019-04-27 18:24:00 +0200

    style booking section - desktop

--- components/Home/SectionBooking.vue
+++ components/Home/SectionBooking.vue
@@ -3,33 +3,45 @@
     <div class="container">
       <div class="booking__content">
         <div class="booking__text">
-          <h2>Make Stilo Your Number One Choice</h2>
+          <h2>Make Stilo Your <br> Number One Choice</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, ipsa. Officiis exercitationem sit non dicta?</p>
-          <div class="booking__cta">
-            <base-button primary>
-              Book an appointment
-            </base-button>
-            <base-button>contact us</base-button>
-          </div>
         </div>
-        <img src="~/assets/images/cta.png">
+        <div class="booking__cta">
+          <base-button primary>
+            Book an appointment
+          </base-button>
+          <base-button>contact us</base-button>
+        </div>
       </div>
     </div>
+    <div class="bg-image" />
   </section>
 </template>
 
 <style lang="scss" scoped>
+.bg-image {
+  background-image: url('~assets/images/cta.png');
+  background-position: center center;
+  background-size: cover;
+  position: absolute;
+  width: 65%;
+  top: 0;
+  right: 0;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.5;
+}
+
 .booking {
-  padding: 0;
-  background-color: red;
+  background-color: #f6f9f9;
+  position: relative;
+  z-index: 1;
 
   &__content {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
   }
 
   &__text {
-    margin-top: 8rem;
+    margin-bottom: 4rem;
   }
 
   &__cta {
