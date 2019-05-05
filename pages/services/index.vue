7741759b189fbd6973c922542ba9b29c941b4931
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2019-05-05 12:45:09 +0200

    add services page

---
+++ pages/services/index.vue
@@ -0,0 +1,191 @@
+<template>
+  <div>
+    <page-header>Services</page-header>
+    <div class="service">
+      <div class="service__card">
+        <div class="service__details">
+          <h2>Hair Cuts & Style</h2>
+          <p>
+            <span>Women's Haircut:</span>
+            <span class="service__price">
+              $55+
+            </span>
+          </p>
+          <p>
+            <span>Men's Haircut:</span>
+            <span class="service__price">
+              $75+
+            </span>
+          </p>
+          <p>
+            <span>Up Do Style:</span>
+            <span class="service__price">
+              $35+
+            </span>
+          </p>
+          <p>
+            <span>Half-up do style:</span>
+            <span class="service__price">
+              $45+
+            </span>
+          </p>
+        </div>
+      </div>
+      <div class="jarallax">
+        <img
+          class="jarallax-img"
+          src="~/assets/images/30.jpg"
+          alt=""
+        >
+      </div>
+    </div>
+    <div class="service">
+      <div class="jarallax">
+        <img
+          class="jarallax-img"
+          src="~/assets/images/31.jpg"
+          alt=""
+        >
+      </div>
+      <div class="service__card">
+        <div class="service__details">
+          <h2>Hair dyed</h2>
+          <p>
+            <span>Dye L'Oréal:</span>
+            <span class="service__price">
+              $55+
+            </span>
+          </p>
+          <p>
+            <span>Dye Davines:</span>
+            <span class="service__price">
+              $75+
+            </span>
+          </p>
+          <p>
+            <span>Dye GolDWell:</span>
+            <span class="service__price">
+              $35+
+            </span>
+          </p>
+          <p>
+            <span>Dye Oriental:</span>
+            <span class="service__price">
+              $45+
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="service">
+      <div class="service__card">
+        <div class="service__details">
+          <h2>Hair Care</h2>
+          <p>
+            <span>Shampoo & Blow Dry:</span>
+            <span class="service__price">
+              $55+
+            </span>
+          </p>
+          <p>
+            <span>Personalized Color:</span>
+            <span class="service__price">
+              $75+
+            </span>
+          </p>
+          <p>
+            <span> Formal Styling:</span>
+            <span class="service__price">
+              $35+
+            </span>
+          </p>
+          <p>
+            <span>Bridal & Wedding:</span>
+            <span class="service__price">
+              $45+
+            </span>
+          </p>
+        </div>
+      </div>
+      <div class="jarallax">
+        <img
+          class="jarallax-img"
+          src="~/assets/images/32.jpg"
+          alt=""
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PageHeader from '~/components/Shared/PageHeader/PageHeader'
+
+export default {
+  components: {
+    PageHeader
+  },
+  mounted() {
+    if (process.browser) {
+      const jarallax = require('jarallax').jarallax
+      jarallax(document.querySelectorAll('.jarallax'), {
+        speed: 0.3
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.service {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  &__card {
+    height: 50rem;
+    display: flex;
+    align-items: center;
+  }
+
+  &__details {
+    width: 57rem;
+    margin-left: auto;
+    padding-left: 2rem;
+
+    h2 {
+      margin-bottom: 4rem;
+    }
+
+    p {
+      margin-bottom: 3rem;
+      max-width: 25rem;
+      display: flex;
+      justify-content: space-between;
+    }
+
+    @media screen and (max-width: 1160px) {
+      margin-left: 0;
+      width: auto;
+    }
+  }
+
+  &__price {
+    font-weight: bold;
+    color: #252525;
+  }
+}
+
+.jarallax {
+  position: relative;
+  z-index: 0;
+}
+.jarallax > .jarallax-img {
+  position: absolute;
+  object-fit: cover;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+}
+</style>
